456bcbce93f12f47900ab94f6d5fe3b2e79387a3
AngelikaKh <angelika1mos@gmail.com>
2021-04-17 19:06:46 +0400

    classes

--- MyPlayground.playground/Pages/Untitled Page 3.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page 3.xcplaygroundpage/Contents.swift
@@ -2,3 +2,31 @@
 
 import Foundation
 
+class Cars {
+    
+    let classification = "Vehicle"
+    var fuelSource = "diesel"
+    let wheels = 4
+    let axies = 2
+    let price = 0
+    
+    func canFly() -> Bool {
+        return false
+    }
+}
+
+class BMW: Cars {
+    
+    let wings = 2
+    
+    override func canFly() -> Bool {
+        return true
+    }
+    
+}
+
+let myBMW = BMW()
+myBMW.canFly()
+myBMW.fuelSource
+print(myBMW)
+
