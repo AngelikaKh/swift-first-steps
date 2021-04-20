e69e9f32180c281ed9850248d1227ba9657b1589
AngelikaKh <angelika1mos@gmail.com>
2021-04-20 12:59:46 +0400

    hackerrank 7. array.

--- MyPlayground.playground/Pages/hackerRank7.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/hackerRank7.xcplaygroundpage/Contents.swift
@@ -1,7 +1,25 @@
 //: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+var arr = [1, 4, 3, 2]
 
-//: [Next](@next)
+for i in arr.reversed() {
+    print(i, terminator: " ")
+}
+
+
+//Task
+//Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+//
+//Example
+//
+//Print 4 3 2 1. Each integer is separated by one space.
+//
+//Input Format
+//
+//The first line contains an integer,  (the size of our array).
+//The second line contains  space-separated integers that describe array 's elements.
+//
+//Output Format
+//
+//Print the elements of array  in reverse order as a single line of space-separated numbers.
