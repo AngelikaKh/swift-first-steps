6c315266ebf17b8ffe04421f39479ea29d724f0f
AngelikaKh <angelika1mos@gmail.com>
2021-04-20 09:37:23 +0400

    hackeRank6, vers2

---
+++ MyPlayground.playground/Pages/hackerRank6 v2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+//: [Previous](@previous)
+
+import Foundation
+import Darwin
+
+func printEvenAndOdd(string: String) {
+    
+    for (i, chr) in string.enumerated() {
+        guard i % 2 == 0 else { continue }
+        print(chr, terminator: "")
+    }
+    
+    print(" ", terminator: "")
+    
+    for (i, chr) in string.enumerated() {
+        guard i % 2 != 0 else { continue }
+        print(chr, terminator: "")
+    }
+    
+    print()
+}
+
+
+let inputString1 = "Hacker"
+let inputString2 = "Rank"
+
+printEvenAndOdd(string: inputString1) // Hce akr
+printEvenAndOdd(string: inputString2) // Rn ak
