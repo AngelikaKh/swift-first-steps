977136e88a6f06a2bf73bd64bfb5f58697250cfa
AngelikaKh <angelika1mos@gmail.com>
2021-04-21 17:25:17 +0400

    hackerrank8

---
+++ MyPlayground.playground/Pages/hackerrank8.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/// Create Phone Book
+for _ in 0..<n {
+    let user = readLine()!
+    let data = user.split(separator: " ").compactMap { String($0) }
+
+    phoneBook[data[0]] = data[1]
+}
+
+// Print the associated entry from phone book on a new line in the form name=phoneNumber,
+// if an entry is not found, print Not found instead.
+for _ in 0..<n {
+    guard let name = readLine() else { break }
+    if let phone = phoneBook[name] {
+        print("\(name)=\(phone)")
+    } else {
+        print("Not found")
+    }
+}
