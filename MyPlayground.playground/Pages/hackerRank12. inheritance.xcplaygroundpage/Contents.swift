bf0d94495492be9a217e76a2d6b046ff9d2bc3e4
AngelikaKh <angelika1mos@gmail.com>
2021-04-26 21:31:32 +0400

    hackerrank12

--- MyPlayground.playground/Pages/hackerRank12. inheritance.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/hackerRank12. inheritance.xcplaygroundpage/Contents.swift
@@ -1,5 +1,72 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var str = "Hello, playground"
+// Class Person
+class Person {
+    private let firstName: String
+    private let lastName: String
+    private let id: Int
+
+    // Initializer
+    init(firstName: String, lastName: String, id: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.id = id
+    }
+
+    // Print person data
+    func printPerson() {
+        print("Name: \(lastName), \(firstName)")
+        print("ID: \(id)")
+    }
+} // End of class Person
+class Student: Person {
+
+    var testScores: [Int]
+
+    /// Student Initializer
+    ///
+    /// - Parameters:
+    ///   - firstName: A string denoting the Person's first name.
+    ///   - lastName: A string denoting the Person's last name.
+    ///   - id: An integer denoting the Person's ID number.
+    ///   - scores: An array of integers denoting the Person's test scores.
+    init(firstName: String, lastName: String, id: Int, scores: [Int]) {
+        testScores = scores
+        super.init(firstName: firstName, lastName: lastName, id: id)
+    }
+
+    /// Calculates a Student average grade
+    ///
+    /// - Returns: A character denoting the grade.
+    func calculate() -> Character {
+        let average = Double(testScores.reduce(0, +) / testScores.count)
+
+        switch average {
+        case ...39:
+            return "T"
+        case 40...54:
+            return "D"
+        case 55...69:
+            return "P"
+        case 70...79:
+            return "A"
+        case 80...89:
+            return "E"
+        case 90...100:
+            return "O"
+        default:
+            return "U" // Undefined
+        }
+    }
+
+}
+
+let nameAndID = readLine()!.components(separatedBy: " ")
+let _ = readLine()
+let scores = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+let s = Student(firstName: nameAndID[0], lastName: nameAndID[1], id: Int(nameAndID[2])!, scores: scores)
+
+s.printPerson()
+
+print("Grade: \(s.calculate())")
