6472884e465c667471d5d5df310e1da35629799c
AngelikaKh <angelika1mos@gmail.com>
2021-04-29 22:29:10 +0400

    autoclosure

--- MyPlayground.playground/Pages/hackerrank13.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/hackerrank13.xcplaygroundpage/Contents.swift
@@ -1,22 +1,38 @@
 import Foundation
 
-func someFunctionWithNonescapingClosure(closure: () -> Void) {
-    closure()
-}
+//func someFunctionWithNonescapingClosure(closure: () -> Void) {
+//    closure()
+//}
+//
+//class SomeClass {
+//    var x = 10
+//    func doSomething() {
+////        someFunctionWithEscapingClosure { self.x = 100 }
+//        someFunctionWithNonescapingClosure { x = 200 }
+//    }
+//}
+//
+//let instance = SomeClass()
+//instance.doSomething()
+//print(instance.x)
+//// Prints "200"
+//
+//completionHandlers.first?()
+//print(instance.x)
+//// Prints "100"
 
-class SomeClass {
-    var x = 10
-    func doSomething() {
-//        someFunctionWithEscapingClosure { self.x = 100 }
-        someFunctionWithNonescapingClosure { x = 200 }
-    }
-}
 
-let instance = SomeClass()
-instance.doSomething()
-print(instance.x)
-// Prints "200"
+//autoclosure
 
-completionHandlers.first?()
-print(instance.x)
-// Prints "100"
+var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+print(customersInLine.count)
+// Prints "5"
+
+let customerProvider = { customersInLine.remove(at: 0) }
+print(customersInLine.count)
+// Prints "5"
+
+print("Now serving \(customerProvider())!")
+// Prints "Now serving Chris!"
+print(customersInLine.count)
+// Prints "4"
